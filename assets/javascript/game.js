2bbee8579d36a5b59fe9194a3221718827c9c76c
ZacWarner <zektgn@gmail.com>
2019-03-24 02:06:42 -0700

    core game functions. began work on scoreboard.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,45 @@
-function myFunction() {
-    document.getElementById("directions").innerHTML = "Ok First we need to Pick a word.";
-}
+var word;
+var wordArray = [];
+var boardArray = [];
+var wordLength;
+var guesses = 0;
+var wins = 0;
+var losses = 0;
+guessedLettersArray = [];
 
 function getWord() {
+    console.log("getWord start")
     var word = prompt("OK enter a word.");
-    document.getElementById("directions").innerHTML = "Ok you chose " + word + " as your word!";
+    alert("Ok you chose " + word + " as your word!");
+    word = word.toLowerCase();
+    return word;
+}
+
+function getLength() {
+    console.log("Getlength start");
+    wordLength = word.length;
+    return wordLength;
 }
 
 function setUpGame() {
-    var word = "ironman";
-    var wordArray = [];
-    var wordLength = word.length;
+    console.log("setUpGame start");
     for (var i = 0; i < wordLength; i++) {
-        wordArray[i] = "_";
+        this.boardArray[i] = "_";
     }
+    return boardArray;
+}
 
+function wordIntoArray() {
+    for (var i = 0; i < word.length; i++) {
+        wordArray[i] = word.charAt(i);
+    }
+}
+
+function upDateBoard() {
+    console.log("upDateBoard start");
     text = '<ul class="list-group list-group-horizontal-md">';
     for (i = 0; i < wordLength; i++) {
-        text += '<li class="list-group-item">' + wordArray[i] + '</li>';
+        text += '<li class="list-group-item">' + boardArray[i] + '</li>';
     }
     text += "</ul>";
 
@@ -25,29 +47,54 @@ function setUpGame() {
 }
 
 function letterGuess() {
-    var word = "ironman";
-    var wordArray = ["_", "_", "_", "_", "_", "_", "_",];
-    var wordLength = word.length;
+    console.log("letterGuess start");
+    alert("Pick first guess!");
+
     var letterTest = 1;
     document.onkeyup = function (event) {
         var userGuess = event.key;
         console.log(userGuess);
-
-        letterTest = word.indexOf(userGuess);
-
-        if (letterTest >= -1) {
-            wordArray[letterTest] = userGuess;
+        letterTest = wordArray.indexOf(userGuess);
+        if (letterTest === -1) {
+            guesses++;
+            guessedLettersArray.push(userGuess);
         }
-
-        text = '<ul class="list-group list-group-horizontal-md">';
-        for (i = 0; i < wordLength; i++) {
-            text += '<li class="list-group-item">' + wordArray[i] + '</li>';
+        else {
+            for (var i = 0; i < wordLength; i++) {
+                letterTest = wordArray.indexOf(userGuess);
+                if (letterTest >= -1) {
+                    boardArray[letterTest] = userGuess;
+                    wordArray[letterTest] = 0;
+                }
+            }
         }
-        text += "</ul>";
-
-        document.getElementById("directions").innerHTML = text;
+        upDateBoard();
+        console.log(guesses);
+        console.log(guessedLettersArray);
     }
 
+
+}
+
+function gameStart() {
+
+    word = getWord();
+    console.log(word);
+
+    wordLength = getLength();
+    console.log(wordLength);
+
+    wordIntoArray();
+    console.log(wordArray);
+
+    boardArray = setUpGame();
+    console.log(boardArray.toString());
+
+    upDateBoard();
+
+    letterGuess();
+
+    // upDateBoard();
 
 }
 
